d0dd465e5008b893787820c72a89b15c888e8ef0
marcandy <marcandy.nj@gmail.com>
2016-11-12 00:17:01 -0600

    what I have so far

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function typeSafe( func, argTypes, returnType ) {
+  for (var i = 0; i < argTypes.length; i++) {
+    if(argTypes[i] == returnType) {
+      return "invalid type"
+    }
+  }
+  //count number of paramater passed
+  if(func.length !== 2) {
+    return "incorrect number of argument";
+  }
+      //if arguments not equal number
+    return func();
 
-}
+  }
+
+
 
 module.exports = typeSafe;
